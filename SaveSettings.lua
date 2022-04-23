fdcf8d460962279834fead0f7fcaf0f16e80fb68
thecriticizedgamer <82607444+thecriticizedgamer@users.noreply.github.com>
2022-04-23 12:12:58 +0200

    Update SaveSettings.lua

--- SaveSettings.lua
+++ SaveSettings.lua
@@ -5,404 +5,153 @@ local string, math, table, Color3, tonumber, tostring = string, math, table, Col
 local type, typeof = type, typeof
 
 
-local readfile = readfile
-local writefile = writefile
 
-local settings = {}
+local encoder = {}
 
 
-local function existsFile(name)
-    return pcall(function()
-        return readfile(name)
-    end)
+	
+local function hex_encode(IN, len)
+    local B,K,OUT,I,D=16,"0123456789ABCDEF","",0,nil
+    while IN>0 do
+        I=I+1
+        IN,D=math.floor(IN/B), IN%B+1
+        OUT=string.sub(K,D,D)..OUT
+    end
+    if len then
+        OUT = ('0'):rep(len - #OUT) .. OUT
+    end
+    return OUT
+end
+local function hex_decode(IN) 
+    return tonumber(IN, 16) 
 end
 
-local function mergetab(a,b)
-    local c = a or {}
-    for i,v in pairs(b or {}) do 
-        c[i] = v 
+local types = {
+    ["nil"] = "0";
+    ["boolean"] = "1";
+    ["number"] = "2";
+    ["string"] = "3";
+    ["table"] = "4";
+
+    ["Vector3"] = "5";
+    ["CFrame"] = "6";
+    ["Instance"] = "7";
+
+    ["Color3"] = "8";
+}
+local rtypes = (function()
+    local a = {}
+    for i,v in pairs(types) do
+        a[v] = i
+    end
+    return a
+end)()
+
+local typeof = typeof or type
+function encoder.encode(t, ...)
+    local type = typeof(t)
+    local s = types[type]
+    local c = ''
+    if type == "nil" then
+        c = types[type] .. "0"
+    elseif type == "boolean" then
+        local t = t == true and '1' or '0'
+        c = s .. t
+    elseif type == "number" then
+        local new = tostring(t)
+        local len = #new
+        c = s .. len .. "." .. new
+    elseif type == "string" then
+        local new = t
+        local len = #new
+        c = s .. len .. "." .. new
+    elseif type == "Vector3" then
+        local x,y,z = tostring(t.X), tostring(t.Y), tostring(t.Z)
+        local new = hex_encode(#x, 2) .. x .. hex_encode(#y, 2) .. y .. hex_encode(#z, 2) .. z
+        c = s .. new
+    elseif type == "CFrame" then
+        local a = {t:GetComponents()}
+        local new = ''
+        for i,v in pairs(a) do
+            local l = tostring(v)
+            new = new .. hex_encode(#l, 2) .. l
+        end
+        c = s .. new
+    elseif type == "Color3" then
+        local a = {t.R, t.G, t.B}
+        local new = ''
+        for i,v in pairs(a) do
+            local l = tostring(v)
+            new = new .. hex_encode(#l, 2) .. l
+        end
+        c = s .. new
+    end
+    return c
+end
+function encoder.decode(t, extra)
+    local p = 0
+    local function read(l)
+        l = l or 1
+        p = p + l
+        return t:sub(p-l + 1, p)
+    end
+    local function get(a)
+        local k = ""
+        while p < #t do
+            if t:sub(p+1,p+1) == a then
+                break
+            else
+                k = k .. read()
+            end
+        end
+        return k
+    end
+    local type = rtypes[read()]
+    local c
+
+    if type == "nil" then
+        read()
+    elseif type == "boolean" then
+        local d = read()
+        c = d == "1" and true or false
+    elseif type == "number" then
+        local length = tonumber(get("."))
+        local d = read(length+1):sub(2,-1)
+        c = tonumber(d)
+    elseif type == "string" then
+        local length = tonumber(get(".")) --read()
+        local d = read(length+1):sub(2,-1)
+        c = d
+    elseif type == "Vector3" then
+        local function getnext()
+            local length = hex_decode(read(2))
+            local a = read(tonumber(length))
+            return tonumber(a)
+        end
+        local x,y,z = getnext(),getnext(),getnext()
+        c = Vector3.new(x, y, z)
+    elseif type == "CFrame" then
+        local a = {}
+        for i = 1,12 do
+            local l = hex_decode(read(2))
+            local b = read(tonumber(l))
+            a[i] = tonumber(b)
+        end
+        c = CFrame.new(unpack(a))
+    elseif type == "Instance" then
+        local pos = hex_decode(read(2))
+        c = extra[tonumber(pos)]
+    elseif type == "Color3" then
+        local a = {}
+        for i = 1,3 do
+            local l = hex_decode(read(2))
+            local b = read(tonumber(l))
+            a[i] = tonumber(b)
+        end
+        c = Color3.new(unpack(a))
     end
     return c
 end
 
 
-local serialize
-local deserialize
-do
-    --/ Serializer : garbage : slow as fuck
-	
-	local function hex_encode(IN, len)
-	    local B,K,OUT,I,D=16,"0123456789ABCDEF","",0,nil
-	    while IN>0 do
-	        I=I+1
-	        IN,D=math.floor(IN/B), IN%B+1
-	        OUT=string.sub(K,D,D)..OUT
-	    end
-		if len then
-			OUT = ('0'):rep(len - #OUT) .. OUT
-		end
-	    return OUT
-	end
-	local function hex_decode(IN) 
-		return tonumber(IN, 16) 
-	end
-
-    local types = {
-        ["nil"] = "0";
-        ["boolean"] = "1";
-        ["number"] = "2";
-        ["string"] = "3";
-        ["table"] = "4";
-
-		["Vector3"] = "5";
-		["CFrame"] = "6";
-        ["Instance"] = "7";
-	
-		["Color3"] = "8";
-    }
-    local rtypes = (function()
-        local a = {}
-        for i,v in pairs(types) do
-            a[v] = i
-        end
-        return a
-    end)()
-
-    local typeof = typeof or type
-    local function encode(t, ...)
-        local type = typeof(t)
-        local s = types[type]
-        local c = ''
-        if type == "nil" then
-            c = types[type] .. "0"
-        elseif type == "boolean" then
-            local t = t == true and '1' or '0'
-            c = s .. t
-        elseif type == "number" then
-            local new = tostring(t)
-            local len = #new
-            c = s .. len .. "." .. new
-        elseif type == "string" then
-            local new = t
-            local len = #new
-            c = s .. len .. "." .. new
-		elseif type == "Vector3" then
-			local x,y,z = tostring(t.X), tostring(t.Y), tostring(t.Z)
-			local new = hex_encode(#x, 2) .. x .. hex_encode(#y, 2) .. y .. hex_encode(#z, 2) .. z
-			c = s .. new
-		elseif type == "CFrame" then
-			local a = {t:GetComponents()}
-			local new = ''
-			for i,v in pairs(a) do
-				local l = tostring(v)
-				new = new .. hex_encode(#l, 2) .. l
-			end
-			c = s .. new
-		elseif type == "Color3" then
-			local a = {t.R, t.G, t.B}
-			local new = ''
-			for i,v in pairs(a) do
-				local l = tostring(v)
-				new = new .. hex_encode(#l, 2) .. l
-			end
-			c = s .. new
-        elseif type == "table" then
-            return serialize(t, ...)
-        end
-        return c
-    end
-    local function decode(t, extra)
-        local p = 0
-        local function read(l)
-            l = l or 1
-            p = p + l
-            return t:sub(p-l + 1, p)
-        end
-        local function get(a)
-            local k = ""
-            while p < #t do
-                if t:sub(p+1,p+1) == a then
-                    break
-                else
-                    k = k .. read()
-                end
-            end
-            return k
-        end
-        local type = rtypes[read()]
-        local c
-
-        if type == "nil" then
-            read()
-        elseif type == "boolean" then
-            local d = read()
-            c = d == "1" and true or false
-        elseif type == "number" then
-            local length = tonumber(get("."))
-            local d = read(length+1):sub(2,-1)
-            c = tonumber(d)
-        elseif type == "string" then
-            local length = tonumber(get(".")) --read()
-            local d = read(length+1):sub(2,-1)
-            c = d
-		elseif type == "Vector3" then
-			local function getnext()
-				local length = hex_decode(read(2))
-				local a = read(tonumber(length))
-				return tonumber(a)
-			end
-			local x,y,z = getnext(),getnext(),getnext()
-			c = Vector3.new(x, y, z)
-		elseif type == "CFrame" then
-			local a = {}
-			for i = 1,12 do
-				local l = hex_decode(read(2))
-				local b = read(tonumber(l))
-				a[i] = tonumber(b)
-			end
-			c = CFrame.new(unpack(a))
-        elseif type == "Instance" then
-			local pos = hex_decode(read(2))
-			c = extra[tonumber(pos)]
-		elseif type == "Color3" then
-			local a = {}
-			for i = 1,3 do
-				local l = hex_decode(read(2))
-				local b = read(tonumber(l))
-				a[i] = tonumber(b)
-			end
-			c = Color3.new(unpack(a))
-        end
-        return c
-    end
-
-    function serialize(data, p)
-		if data == nil then return end
-        local type = typeof(data)
-        if type == "table" then
-            local extra = {}
-            local s = types[type]
-            local new = ""
-            local p = p or 0
-            for i,v in pairs(data) do
-                local i1,v1
-                local t0,t1 = typeof(i), typeof(v)
-
-				local a,b
-                if t0 == "Instance" then
-                    p = p + 1
-                    extra[p] = i
-                    i1 = types[t0] .. hex_encode(p, 2)
-                else
-                    i1, a = encode(i, p)
-					if a then
-						for i,v in pairs(a) do
-							extra[i] = v
-						end
-					end
-                end
-                
-                if t1 == "Instance" then
-                    p = p + 1
-                    extra[p] = v
-                    v1 = types[t1] .. hex_encode(p, 2)
-                else
-                    v1, b = encode(v, p)
-					if b then
-						for i,v in pairs(b) do
-							extra[i] = v
-						end
-					end
-                end
-                new = new .. i1 .. v1
-            end
-            return s .. #new .. "." .. new, extra
-		elseif type == "Instance" then
-			return types[type] .. hex_encode(1, 2), {data}
-        else
-            return encode(data), {}
-        end
-    end
-
-    function deserialize(data, extra)
-		if data == nil then return end
-		extra = extra or {}
-		
-        local type = rtypes[data:sub(1,1)]
-        if type == "table" then
-
-            local p = 0
-            local function read(l)
-                l = l or 1
-                p = p + l
-                return data:sub(p-l + 1, p)
-            end
-            local function get(a)
-                local k = ""
-                while p < #data do
-                    if data:sub(p+1,p+1) == a then
-                        break
-                    else
-                        k = k .. read()
-                    end
-                end
-                return k
-            end
-
-            local length = tonumber(get("."):sub(2, -1))
-            read()
-
-            local new = {}
-
-            local l = 0
-            while p <= length do
-                l = l + 1
-
-				local function getnext()
-					local i
-                    local t = read()
-                    local type = rtypes[t]
-
-                    if type == "nil" then
-                        i = decode(t .. read())
-                    elseif type == "boolean" then
-                        i = decode(t .. read())
-                    elseif type == "number" then
-                        local l = get(".")
-                        
-                        local dc = t .. l .. read()
-                        local a = read(tonumber(l))
-                        dc = dc .. a
-
-                        i = decode(dc)
-                 	elseif type == "string" then
-                        local l = get(".")
-                        local dc = t .. l .. read()
-                        local a = read(tonumber(l))
-                        dc = dc .. a
-
-                        i = decode(dc)
-					 elseif type == "Vector3" then
-						local function getnext()
-							local length = hex_decode(read(2))
-							local a = read(tonumber(length))
-							return tonumber(a)
-						end
-						local x,y,z = getnext(),getnext(),getnext()
-						i = Vector3.new(x, y, z)
-					elseif type == "CFrame" then
-						local a = {}
-						for i = 1,12 do
-							local l = hex_decode(read(2))
-							local b = read(tonumber(l)) -- why did I decide to do this
-							a[i] = tonumber(b)
-						end
-						i = CFrame.new(unpack(a))
-					elseif type == "Instance" then
-						local pos = hex_decode(read(2))
-						i = extra[tonumber(pos)]
-					elseif type == "Color3" then
-						local a = {}
-						for i = 1,3 do
-							local l = hex_decode(read(2))
-							local b = read(tonumber(l))
-							a[i] = tonumber(b)
-						end
-						i = Color3.new(unpack(a))
-                    elseif type == "table" then
-                        local l = get(".")
-                        local dc = t .. l .. read() .. read(tonumber(l))
-                        i = deserialize(dc, extra)
-                    end
-					return i
-				end
-                local i = getnext()
-                local v = getnext()
-
-               new[(typeof(i) ~= "nil" and i or l)] =  v
-            end
-
-
-            return new
-		elseif type == "Instance" then
-			local pos = tonumber(hex_decode(data:sub(2,3)))
-			return extra[pos]
-        else
-            return decode(data, extra)
-        end
-    end
-end
-
-
--- great you have come a far way now stop before my horrible scripting will infect you moron
-
-do
-    --/ Settings
-
-    -- TODO: Other datatypes.
-    settings.fileName = ""
-    settings.saved = {}
-
-    function settings:Get(name, default)
-        local self = {}
-        local value = settings.saved[name]
-        if value == nil and default ~= nil then
-            value = default
-            settings.saved[name] = value
-        end
-        self.Value = value
-        function self:Set(val)
-            self.Value = val
-            settings.saved[name] = val
-        end
-        return self  --value or default
-    end
-
-    function settings:Set(name, value)
-        print(settings.saved)
-        local r = settings.saved[name]
-        settings.saved[name] = value
-        return r
-    end
-
-    function settings:Save()
-        local savesettings = settings:GetAll() or {}
-        local new = mergetab(savesettings, settings.saved)
-        local js = serialize(new)
-
-        writefile(settings.fileName, js)
-    end
-
-    function settings:GetAll()
-        if not existsFile(settings.fileName) then
-            return
-        end
-        local fileContents = readfile(settings.fileName)
-
-        local data
-        pcall(function()
-            data = deserialize(fileContents)
-        end)
-        return data
-    end
-
-    function settings:Load()
-        if not existsFile(settings.fileName) then
-            return
-        end
-        local fileContents = readfile(settings.fileName)
-
-        local data
-        pcall(function()
-            data = deserialize(fileContents)
-        end)
-
-        if data then
-            data = mergetab(settings.saved, data)
-        end
-        settings.saved = data
-        return data
-    end
-    
-end
-return settings
+return encoder
